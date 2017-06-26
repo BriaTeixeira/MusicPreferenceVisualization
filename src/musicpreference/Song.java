eaf9400bb2b85d53240a9e522596244334137af5
BriaTeixeira <bria96@vt.edu>
2017-06-26 19:14:20 -0400

    Add files via upload
    Song class.

--- src/musicpreference/Song.java
+++ src/musicpreference/Song.java
@@ -4,10 +4,136 @@
 package musicpreference;
 
 /**
+ * Store the information specific to different songs as well as provide
+ * the functionality to get specific information and compare songs to
+ * each other.
  * @author Bria
  *
  */
 public class Song
 {
+    private String artist;
+    private String title;
+    private String genre;
+    private int year;
+    private final int column;
+    
+    /**
+     * Create a new song object.
+     * Each song constructor should assign the song's title,
+     * artist, genre, and release year values to the
+     * appropriate field as well as assign a column number
+     * to use when storing data.
+     * @param title The song's title.
+     * @param artist The song's artist.
+     * @param genre The song's genre.
+     * @param year The year the song was released.
+     * @param col The column we want the data to be stored in.
+     */
+    public Song(String title, String artist, String genre,
+            int year, int col)
+    {
+        this.title = title;
+        this.artist = artist;
+        this.genre = genre;
+        this.year = year;
+        
+        column = col; // Find a way to determine column number
+    }
+    
+    // ----------------------------------------------------------------
+    
+    /**
+     * Return the column index at which the data relevant to this song
+     * will be stored.
+     * @return the column index.
+     */
+    public int getCol()
+    {
+        return column;
+    }
+    /**
+     * Get the song's Artist.
+     * @return a string of the artist's name.
+     */
+    public String getArtist()
+    {
+        return artist;
+    }
+    
+    /**
+     * Get the song's Title.
+     * @return a string of the song's title.
+     */
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    /**
+     * Get the song's Genre.
+     * @return a string of the song's genre.
+     */
+    public String getGenre()
+    {
+        return genre;
+    }
+    
+    /**
+     * Get the song's release year.
+     * @return an integer representing the song's release year.
+     */
+    public int getYear()
+    {
+        return year;
+    }
+    
+    /**
+     * Compare two song's based on artist names.
+     * @param song The song object we are comparing to.
+     * @return a number less than 0 if the relationship is "less than",
+     * a number greater than 0 if the relationship is "greater than", 
+     * and 0 if the artist names are the same.
+     */
+    public int compareArtist(Song song)
+    {
+        return this.getArtist().compareTo(song.getArtist());
+    }
+    
+    /**
+     * Compare two song's based on titles.
+     * @param song The song object we are comparing to.
+     * @return a number less than 0 if the relationship is "less than",
+     * a number greater than 0 if the relationship is "greater than", 
+     * and 0 if the titles are the same.
+     */
+    public int compareTitle(Song song)
+    {
+        return this.getTitle().compareTo(song.getTitle());
+    }
+    
+    /**
+     * Compare two song's based on genre.
+     * @param song The song object we are comparing to.
+     * @return a number less than 0 if the relationship is "less than",
+     * a number greater than 0 if the relationship is "greater than", 
+     * and 0 if the genres are the same.
+     */
+    public int compareGenre(Song song)
+    {
+        return this.getGenre().compareTo(song.getGenre());
+    }
+    
+    /**
+     * Compare two song's based on release year.
+     * @param song The song object we are comparing to.
+     * @return a number less than 0 if the relationship is "less than",
+     * a number greater than 0 if the relationship is "greater than", 
+     * and 0 if the release years are the same.
+     */
+    public int compareYear(Song song)
+    {
+        return this.getYear() - song.getYear();
+    }
 
 }
