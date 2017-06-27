ef3d514679a0ae20c877b6399122e5e813beda21
fanyy94 <fanyy94@vt.edu>
2017-06-26 21:25:24 -0400

    Add files via upload

--- src/musicpreference/SongList.java
+++ src/musicpreference/SongList.java
@@ -9,5 +9,92 @@ package musicpreference;
  */
 public class SongList extends LinkedList<Song>
 {
+    /**
+     * constructor
+     */
+    public SongList()
+    {
+        super();
+    }
 
+    /**
+     * sort by title
+     */
+    public void sortTitle()
+    {
+        if (this.getLength() > 1)
+        {
+            for (int i = 1; i <this.getLength(); i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (this.getEntry(j).compareTitle(this.getEntry(j-1)) < 0)
+                    {
+                        swap(j, j-1);
+                    }
+
+                }
+            }
+        }
+    }
+    /**
+     * sort by Artist
+     */
+    public void sortArtist()
+    {
+        if (this.getLength() > 1)
+        {
+            for (int i = 1; i <this.getLength(); i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (this.getEntry(j).compareArtist(this.getEntry(j-1)) < 0)
+                    {
+                        swap(j, j-1);
+                    }
+
+                }
+            }
+        }
+    }
+    /**
+     * sort by Year
+     */
+    public void sortYear()
+    {
+        if (this.getLength() > 1)
+        {
+            for (int i = 1; i <this.getLength(); i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (this.getEntry(j).compareYear(this.getEntry(j-1)) < 0)
+                    {
+                        swap(j, j-1);
+                    }
+
+                }
+            }
+        }
+    }
+    /**
+     * sort by Genre
+     */
+    public void sortGenre()
+    {
+        if (this.getLength() > 1)
+        {
+            for (int i = 1; i <this.getLength(); i++)
+            {
+                for (int j = i; j > 0; j--)
+                {
+                    if (this.getEntry(j).compareGenre(this.getEntry(j-1)) < 0)
+                    {
+                        swap(j, j-1);
+                    }
+
+                }
+            }
+        }
+    }
 }
