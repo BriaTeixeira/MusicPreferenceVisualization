f94656ee789c1e637f40f1b6dfe669808a2563e4
BriaTeixeira <bria96@vt.edu>
2017-06-27 20:08:35 -0400

    Update Bar.java

--- src/musicpreference/Bar.java
+++ src/musicpreference/Bar.java
@@ -11,6 +11,7 @@ import CS2114.Shape;
 public class Bar extends Shape
 {
     public static final int HEIGHT = 25;
+    public static final int MAX_WIDTH = 100;
     /**
      * Create a new Bar shape.
      * @param width The width of the bar -> to be determined by 
